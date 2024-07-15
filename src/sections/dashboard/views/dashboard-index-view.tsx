e52cf5619fd065e1bc1f60dabb938056aeb1e700
gowth6m <gowtham4n@gmail.com>
2024-07-15 18:20:36 +0100

    Add mock content for dashboard

--- src/sections/dashboard/views/dashboard-index-view.tsx
+++ src/sections/dashboard/views/dashboard-index-view.tsx
@@ -1,7 +1,16 @@
 import Row from '@/components/core/row';
 import Column from '@/components/core/column';
 
-import { Card, Divider, Container, CardHeader } from '@mui/material';
+import {
+  Card,
+  List,
+  Divider,
+  ListItem,
+  Container,
+  CardHeader,
+  Typography,
+  ListItemText,
+} from '@mui/material';
 
 const DashboardIndexView = () => {
   return (
@@ -19,7 +28,33 @@ const DashboardIndexView = () => {
               borderStyle: 'dashed',
             }}
           />
-          <Row>Last time</Row>
+          <Row padding={3}>
+            <Column gap={1}>
+              <Typography variant="h6">Last time you discussed</Typography>
+
+              <List dense>
+                <ListItem>
+                  <ListItemText primary="Your career goals" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Your progress against your goals" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Your development plan" />
+                </ListItem>
+              </List>
+            </Column>
+
+            <Column gap={1}>
+              <Typography variant="h6">Last time you discussed</Typography>
+
+              <List dense>
+                <ListItem>
+                  <ListItemText primary="You might have missed apples" />
+                </ListItem>
+              </List>
+            </Column>
+          </Row>
         </Card>
       </Column>
     </Container>
